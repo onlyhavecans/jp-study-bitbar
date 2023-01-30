400a3cc83b7aaad63a300cf568725828e9f0dc76
Amelia Aronsohn <squirrel@wearing.black>
2023-01-29 20:38:54 -0800

    switch to ufmt & flake8

--- bunpro.15m.py
+++ bunpro.15m.py
@@ -5,7 +5,7 @@
 # <bitbar.version>v1.0</bitbar.version>
 # <bitbar.author>Amelia Aronsohn</bitbar.author>
 # <bitbar.author.github>onlyhavecans</bitbar.author.github>
-# <bitbar.desc>Shows available lessons and reviews with links in menu</bitbar.desc>
+# <bitbar.desc>Shows available lessons and reviews with links</bitbar.desc>
 # <bitbar.dependencies>python</bitbar.dependencies>
 
 # To install, you will want to generate an API key and store the
@@ -15,23 +15,25 @@
 import json
 import os
 from urllib.error import HTTPError, URLError
-from urllib.request import urlopen, Request
+from urllib.request import Request, urlopen
 
-API_KEY = os.path.expanduser('~/.config/bunpro.jp/api.key')
-ENDPOINT = 'https://bunpro.jp/api/user/{0}/study_queue'
+API_KEY = os.path.expanduser("~/.config/bunpro.jp/api.key")
+ENDPOINT = "https://bunpro.jp/api/user/{0}/study_queue"
 
 
 def error(message):
-    print('BP: X')
-    print('---')
+    print("BP: X")
+    print("---")
     print(message)
     exit()
 
 
 def get(url, apikey):
     u = url.format(apikey)
-    r = Request(u, method='GET')
-    r.add_header('User-Agent', 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11')
+    r = Request(u, method="GET")
+    r.add_header(
+        "User-Agent", "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"
+    )
     try:
         result = urlopen(r).read()
     except HTTPError as e:
@@ -44,16 +46,16 @@ def get(url, apikey):
 
 def parse_queue(study_data):
     info = {}
-    user = study_data['user_information']
-    info['name'] = user['username']
-    info['ghosts'] = user['ghost_review_count']
-    info['reviews'] = study_data['requested_information']['reviews_available']
+    user = study_data["user_information"]
+    info["name"] = user["username"]
+    info["ghosts"] = user["ghost_review_count"]
+    info["reviews"] = study_data["requested_information"]["reviews_available"]
     return info
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if not os.path.exists(API_KEY):
-        error('Missing API Key')
+        error("Missing API Key")
 
     with open(API_KEY) as key_file:
         key = key_file.read().strip()
@@ -61,11 +63,11 @@ if __name__ == '__main__':
         counts = parse_queue(summary)
 
         print(f"BP: {counts['reviews']}")
-        print('---')
+        print("---")
 
-        print('REVIEWS | size=10')
+        print("REVIEWS | size=10")
         print(f"Reviews - {counts['reviews']} | href=https://bunpro.jp/study")
 
-        print('---')
+        print("---")
         print(f"You have {counts['ghosts']} Ghosts!")
-        print('Lessons | href=https://bunpro.jp/learn')
+        print("Lessons | href=https://bunpro.jp/learn")
--- wanikani.15m.py
+++ wanikani.15m.py
@@ -5,7 +5,7 @@
 # <bitbar.version>v1.0</bitbar.version>
 # <bitbar.author>Amelia Aronsohn</bitbar.author>
 # <bitbar.author.github>onlyhavecans</bitbar.author.github>
-# <bitbar.desc>Shows available lessons and reviews with links in menu</bitbar.desc>
+# <bitbar.desc>Shows available lessons and reviews with links</bitbar.desc>
 # <bitbar.dependencies>python</bitbar.dependencies>
 
 # To install, you will want to generate an API key and store the
@@ -15,23 +15,23 @@
 import json
 import os
 from urllib.error import HTTPError, URLError
-from urllib.request import urlopen, Request
+from urllib.request import Request, urlopen
 
-API_KEY = os.path.expanduser('~/.config/wanikani.com/api.key')
-SUMMARY_ENDPOINT = 'https://api.wanikani.com/v2/summary'
+API_KEY = os.path.expanduser("~/.config/wanikani.com/api.key")
+SUMMARY_ENDPOINT = "https://api.wanikani.com/v2/summary"
 
 
 def error(message):
-    print('WK: X')
-    print('---')
+    print("WK: X")
+    print("---")
     print(message)
     exit()
 
 
 def get(url, apikey):
-    r = Request(url, method='GET')
-    r.add_header('Authorization', f"Bearer {apikey}")
-    r.add_header('Wanikani-Revision', '20170710')
+    r = Request(url, method="GET")
+    r.add_header("Authorization", f"Bearer {apikey}")
+    r.add_header("Wanikani-Revision", "20170710")
     try:
         result = urlopen(r).read()
     except HTTPError as e:
@@ -43,15 +43,15 @@ def get(url, apikey):
 
 
 def parse_counts(study_data):
-    info = study_data['data']
-    lesson_count = len(info['lessons'][0]['subject_ids'])
-    review_count = len(info['reviews'][0]['subject_ids'])
-    return {'lessons': lesson_count, 'reviews': review_count}
+    info = study_data["data"]
+    lesson_count = len(info["lessons"][0]["subject_ids"])
+    review_count = len(info["reviews"][0]["subject_ids"])
+    return {"lessons": lesson_count, "reviews": review_count}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if not os.path.exists(API_KEY):
-        error('Missing API Key')
+        error("Missing API Key")
 
     with open(API_KEY) as key_file:
         key = key_file.read().strip()
@@ -59,9 +59,9 @@ if __name__ == '__main__':
         counts = parse_counts(summary)
 
         print(f"WK: L:{counts['lessons']} R:{counts['reviews']}")
-        print('---')
+        print("---")
 
-        print('LESSONS & REVIEWS | size=10')
+        print("LESSONS & REVIEWS | size=10")
 
         print(f"Lessons - {counts['lessons']} | href=https://www.wanikani.com/lesson")
         print(f"Reviews - {counts['reviews']} | href=https://www.wanikani.com/review")
